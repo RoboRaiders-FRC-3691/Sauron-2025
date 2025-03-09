45308d6a2fe0e8c877d10e9299a5492c095255a0
Shining_Darkness <apgittins@gmail.com>
2025-03-09 00:04:44 -0600

    All function and method defn updated

--- src/main/cpp/subsystems/CoralSubsystem.cpp
+++ src/main/cpp/subsystems/CoralSubsystem.cpp
@@ -12,7 +12,6 @@ void CoralSubsystem::Periodic(){
 
 }
 
-//Command that sets the coral mechanism angle
 frc2::CommandPtr CoralSubsystem::SetAngle(units::angle::turn_t angle){
     return RunOnce([this, angle] {
         if(ValidAngle(angle)){
@@ -21,14 +20,12 @@ frc2::CommandPtr CoralSubsystem::SetAngle(units::angle::turn_t angle){
   });
 }
 
-//Command that sets the intake velocity to a given value
 frc2::CommandPtr CoralSubsystem::SetIntake(units::angular_velocity::revolutions_per_minute_t intakeVelocity){
     return RunOnce([this, intakeVelocity]{
         IntakeMotor.SetControl(m_VelRequest.WithVelocity(intakeVelocity));
   });
 }
 
-//Command that runs the intake at intakeVelocity until interupted
 frc2::CommandPtr CoralSubsystem::RunIntake(units::angular_velocity::revolutions_per_minute_t intakeVelocity){
     return StartEnd(
             [this, intakeVelocity]{
@@ -39,13 +36,10 @@ frc2::CommandPtr CoralSubsystem::RunIntake(units::angular_velocity::revolutions_
 			});
 }
 
-
-//Comnmand that runs the intake at intakeVelocity for "timeout" seconds when called
 frc2::CommandPtr CoralSubsystem::RunIntakeFor(units::angular_velocity::revolutions_per_minute_t intakeVelocity, units::time::second_t timeout){
     return RunIntake(intakeVelocity).WithTimeout(timeout);
 }
 
-//Command that runs the intake at intakeVelocity until the coral limit switch it pressed
 frc2::CommandPtr CoralSubsystem::IntakeWithSensor(units::angular_velocity::revolutions_per_minute_t intakeVelocity){
   return Run([this, intakeVelocity]{
             IntakeMotor.SetControl(m_VelRequest.WithVelocity(intakeVelocity));
--- src/main/include/Utils/Vision/VisionCluster.h
+++ src/main/include/Utils/Vision/VisionCluster.h
@@ -19,6 +19,7 @@
 
 #include <Constants.h>
 
+// Simple constructor to pair an rstimated robot pose and its calculated standard deviations
 struct VisionPoseResult {
     photon::EstimatedRobotPose visionEstimate;
     wpi::array<double, 3U> standardDeviations;
@@ -26,13 +27,19 @@ struct VisionPoseResult {
 
 class VisionCluster{
     public:
-        VisionCluster();
+        // VisionCluser constructor (Creates a vision cluster by loading cameras from deploy/PhotonCameras/)
+        VisionCluster(); // TODO: Add constructor overload or defualt values to enable custom filepaths to cameras directory and alternative field images. (This would increase code reusability)
 
+        // Returns the latest std::vector of vision estimates from the cluster
         std::vector<VisionPoseResult> GetVisionEstimates();
 
+        // Takes an estimated robot pose and filters it returning a vision pose result containing the position, and the position std devs
         std::optional<VisionPoseResult> FilterVisionEstimate(std::optional<photon::EstimatedRobotPose> estimatedPosition);
 
     private:
+        // Define an std::vector to hold the cameras in the cluster
         std::vector<PhotonCamEstimator> m_photonCameras;
+
+        // Define an std::vector of vision estimates
         std::vector<VisionPoseResult> m_visionEst;
 };
