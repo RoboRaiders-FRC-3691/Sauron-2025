136758360c3000d910cbc13d1f543bd365c4b6f0
Aiden <105531847+Shining-Darkness@users.noreply.github.com>
2025-04-02 14:44:46 -0500

    Update ClimberSubsystem.cpp
    Started Climber Cam Stream

--- src/main/cpp/subsystems/ClimberSubsystem.cpp
+++ src/main/cpp/subsystems/ClimberSubsystem.cpp
@@ -5,6 +5,7 @@ using namespace ClimberConstants;
 
 ClimberSubsystem::ClimberSubsystem() : ClimbMotor{kClimberMotorPort, kClimberCANLoop}, m_PoseRequest(0_tr){
     ClimbMotor.GetConfigurator().Apply(kClimberMotorConfigs);
+    m_ClimberCamera = frc::CameraServer::StartAutomaticCapture("ClimberCam Stream", kClimberCameraPort);
 }
 
 void ClimberSubsystem::Periodic(){
